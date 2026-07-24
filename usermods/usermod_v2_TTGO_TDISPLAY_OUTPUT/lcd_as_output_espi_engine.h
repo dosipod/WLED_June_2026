a6dcffa95ab49e9c864f5970e76083954c0c7129
AlDIY <87589371+dosipod@users.noreply.github.com>
2026-07-24 23:50:19 +0300

    Add LcdTfteSpiEngine class for LCD output

---
+++ usermods/usermod_v2_TTGO_TDISPLAY_OUTPUT/lcd_as_output_espi_engine.h
@@ -0,0 +1,46 @@
+#ifndef LCD_AS_OUTPUT_ESPI_ENGINE_H
+#define LCD_AS_OUTPUT_ESPI_ENGINE_H
+
+#include <TFT_eSPI.h>
+
+class LcdTfteSpiEngine {
+  private:
+    TFT_eSPI tft = TFT_eSPI();
+    uint16_t blocksW = 0;
+    uint16_t blocksH = 0;
+    uint16_t blockWidth = 1;
+    uint16_t blockHeight = 1;
+
+  public:
+    void init() {
+      tft.init();
+      tft.setRotation(1);
+      tft.fillScreen(TFT_BLACK);
+    }
+
+    void clear() {
+      tft.fillScreen(TFT_BLACK);
+    }
+
+    void drawFrame(WS2812FX& strip) {
+      if (!strip.isMatrix) return;
+      
+      Segment& seg = strip.getSegment(0);
+      if (seg.width() != blocksW || seg.height() != blocksH) {
+        blocksW = seg.width();
+        blocksH = seg.height();
+        blockWidth  = tft.width() / blocksW;
+        blockHeight = tft.height() / blocksH;
+        tft.fillScreen(TFT_BLACK);
+      }
+
+      for (int h = 0; h < blocksH; h++) {
+        for (int w = 0; w < blocksW; w++) {
+          uint32_t c = strip.getPixelColor(seg.start + w + h * seg.width());
+          tft.fillRect(w * blockWidth, h * blockHeight, blockWidth, blockHeight, tft.color24to16(c));
+        }
+      }
+    }
+};
+
+#endif
