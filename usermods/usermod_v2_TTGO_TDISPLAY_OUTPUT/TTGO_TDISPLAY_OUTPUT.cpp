39fa6d50facacfab47b22fd27de2c1137306342e
AlDIY <87589371+dosipod@users.noreply.github.com>
2026-09-01 14:13:07 +0300

    Change display rotation setting from 1 to 0

--- usermods/usermod_v2_TTGO_TDISPLAY_OUTPUT/TTGO_TDISPLAY_OUTPUT.cpp
+++ usermods/usermod_v2_TTGO_TDISPLAY_OUTPUT/TTGO_TDISPLAY_OUTPUT.cpp
@@ -176,7 +176,7 @@ class TTGO_TDISPLAY_OUTPUT : public Usermod {
   gfx = new Arduino_ST7735(
     bus, 
     pinRst, 
-    1,            // Rotation set to 1 (Landscape)
+    0,            // Rotation set to 1 (Landscape)
     false,        // Standard panel target (Not IPS layout)
     128,          // Physical Canvas width configuration
     128,          // Physical Canvas height configuration
